d07ef8a14e3c78ff4a199f59f257b67e72ebceeb
Naga Sriharsha Mulugu <sriharsha1103@gmail.com>
2026-03-16 09:20:36 +0530

    refactor: restructure user data response in getUser controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -30,7 +30,13 @@ const getUser = async (req, res) => {
     console.log("Fetching all users")
     try {
         const users = await User.find()
-        res.status(200).json(users)
+        res.status(200).json({
+            data: users.map(user => ({
+                id: user._id,
+                username: user.username,
+                role: user.role
+            }))
+        })
     } catch (err) {
         res.status(500).json({ message: 'Error fetching users', error: err.message })
     }
